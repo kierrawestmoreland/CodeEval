d7fa2263e655720388f1e0c18ebe8e05fd045685
kierrawestmoreland <kierra.westmoreland@gmail.com>
2017-01-14 13:08:47 -0500

    FizzBuzz third test

--- src/fizzBuzz/FizzBuzz.java
+++ src/fizzBuzz/FizzBuzz.java
@@ -3,7 +3,13 @@ package fizzBuzz;
 public class FizzBuzz {
 
 	public String fizz(Integer input) {
+		if(input % 3 == 0){
+		return "Fizz";
+		}
+		else
+		{
 		return input.toString();
+		}
 	}
 
 	
--- src/fizzBuzz/FizzBuzzTest.java
+++ src/fizzBuzz/FizzBuzzTest.java
@@ -2,20 +2,30 @@ package fizzBuzz;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
+
+		FizzBuzz fizzBuzz = new FizzBuzz();
+
+	
 	@Test
 	public void shouldReturn1(){
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("1", fizzBuzz.fizz(1));
 	}
 	
 	@Test
-	public void whenFizzBuzzGets2TwoReturn2() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
+	public void whenFizzBuzzIsGiven2TwoReturn2() {
 		assertEquals("2", fizzBuzz.fizz(2));
-		
 	}
+	
+	@Test
+	public void whenFizzBuzzIsGiven3ReturnFizz() {
+		assertEquals("Fizz", fizzBuzz.fizz(3));
+		assertEquals("Fizz", fizzBuzz.fizz(6));
+	}
+	
+	
 }
